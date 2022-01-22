591b3ddc9f9d446c462331e25793296335760bd8
chazbz42cha <cha25.bz42@gmail.com>
2022-01-22 10:56:51 +0100

    patie1

---
+++ index.js
@@ -0,0 +1,86 @@
+// let message = "bonjour"
+// alert (message)
+
+// let nom = prompt("Comment te nomme tu  ?")
+
+// let prenom = prompt("Comment t'appelles-tu ?")
+
+// let ville = prompt("ou habite tu ?")
+
+// alert("Bonjour"+ " "+ nom +" "+ prenom+ " "+ "qui vient de"+" "+ville )
+
+// let nombre = prompt("1er nombre a virgule")
+// let nombre2 = prompt("2eme nombre a virgule")
+
+// let nombre_arrondie = Math.trunc(nombre)
+// let nombre2_arrondie = Math.trunc(nombre2)
+
+// let nombre_par_nombre2 = nombre_arrondie / nombre2_arrondie
+
+// alert(nombre_par_nombre2)
+
+let pointure = prompt("quelle est votre pointure")
+
+let annee_de_naissance = prompt("quelle est votre annee de naissance")
+
+
+annee_de_naissance=parseInt(annee_de_naissance)
+
+alert(pointure * 2 + 5 + annee_de_naissance )
+
+
+
+
+// Exercice 1 :
+// let hello = "Hello world !"
+// alert (hello)
+
+
+//Exercice 2 :
+// let lastName = "Nom"
+// let firstName = "Prenom"
+// let city = "Ville"
+// alert ( "Nom : "+ firstName + "\nPrénom : "+ lastName + "\nVille : "+ city)
+
+
+//Exercice 3 :
+// let Name = prompt("Votre nom ?");
+// alert("Bonjour, " + Name);
+
+
+//Exercice 4 :
+// let lastName = prompt("Quel est votre nom ?");
+// let firstName = prompt("Quel est votre prénom ?");
+// let city = prompt("D'où venez vous ?");
+// alert ( "Bonjour "+ firstName+" " + lastName +" qui vient de "+ city)
+
+//Exercice 5 :
+// let Nombre1 = prompt("Quel est votre premier nombre à virgule ?");
+// let Nombre1Trunc = Math.trunc(Nombre1);
+// let Nombre2 = prompt("Quel est votre deuxieme nombre à virgule ?");
+// let calcul = (Nombre1Trunc * Nombre2)
+// alert ( "Le resultat est : " + calcul)
+
+//Exercice 6 :
+// let Nombre1 = prompt("Quel est votre premier nombre à virgule ?");
+// let Nombre2 = prompt("Quel est votre deuxieme nombre à virgule ?");
+// let calcul = (Nombre1 / Nombre2)
+// alert ( "Le resultat est : " + calcul)
+
+//Exercice 7 :
+// Soluce 1 :
+// let pointure = prompt("votre pointure")
+// let resultatX2 = (pointure * 2)
+// resultat5 = resultatX2 + 5
+// resultat50 = resultat5 * 50
+// resultat = parseInt(resultat50)
+
+// let annee_naissance = prompt("votre année de naissance")
+// annee_naissance = parseInt(annee_naissance)
+// reponse = resultat + annee_naissance
+// parseInt(reponse)
+// alert(reponse)
+
+// Soluce 2 :
+// let naissance = prompt ("quel est votre date de naissance") ;
+//alert ((((Number(pointure) 2)+5 )50) - (naissance) + 1766)
